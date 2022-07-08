89382b7abf113d3f5cef826a68d4c265e27f369a
Luis Almeida <luis.a.almeidajr@gmail.com>
2022-07-08 18:10:33 -0500

    aliases exercise completed

--- aliases_exercises.sql
+++ aliases_exercises.sql
@@ -1,2 +1,8 @@
 USE employees;
 
+SELECT CONCAT(emp_no,' - ',last_name,' ',first_name) AS full_name, birth_date AS DOB
+FROM employees
+GROUP BY full_name, last_name, birth_date
+ORDER BY last_name
+LIMIT 10;
+
