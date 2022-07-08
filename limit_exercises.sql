19f15a612a7465a4fbf2ef2b9e1f28eff8173c52
Luis Almeida <luis.a.almeidajr@gmail.com>
2022-07-08 09:50:19 -0500

    functions exercise started

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -1,7 +1,14 @@
 USE employees;
 
-SELECT DISTINCT title FROM titles;
+SELECT
+DISTINCT title
+FROM titles;
 
-SELECT DISTINCT last_name from employees ORDER BY last_name DESC LIMIT 10;
+SELECT
+DISTINCT last_name
+FROM employees
+ORDER BY last_name DESC LIMIT 10;
 
-SELECT emp_no from salaries ORDER BY  salary DESC LIMIT 5 OFFSET 45;
+SELECT emp_no
+FROM salaries
+ORDER BY  salary DESC LIMIT 5 OFFSET 45;
