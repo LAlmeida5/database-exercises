6858736cfb59af725d9041efb3cf55a75465cb06
Luis Almeida <luis.a.almeidajr@gmail.com>
2022-07-08 10:13:01 -0500

    functions completed

--- functions_exercises.sql
+++ functions_exercises.sql
@@ -6,11 +6,11 @@ WHERE gender = 'M'
 AND (first_name = 'Irena' OR first_name ='Vidya' OR first_name = 'Maya')
 ORDER BY last_name ASC, first_name ASC ;
 
-SELECT *
+
+SELECT CONCAT(first_name,' ',last_name)
 FROM employees
 WHERE last_name LIKE 'E%'
-AND last_name LIKE '%E'
-ORDER BY emp_no;
+AND last_name LIKE '%E';
 
 SELECT *
 FROM employees
@@ -20,3 +20,12 @@ SELECT *
 FROM employees
 WHERE last_name LIKE '%q%'
 AND last_name NOT LIKE '%qu%';
+
+SELECT DATEDIFF(NOW(), hire_date)
+FROM employees
+WHERE year(hire_date) BETWEEN 1990 AND 1999
+AND month(birth_date) = 12
+AND day(birth_date) = 25
+ORDER BY year(hire_date) DESC;
+
+
