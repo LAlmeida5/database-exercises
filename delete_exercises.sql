5e7a701937ac913bbedc44780a45ea35954288e3
Luis Almeida <luis.a.almeidajr@gmail.com>
2022-07-08 10:30:05 -0500

    update

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,1 +1,8 @@
 USE codeup_test_db;
+
+SELECT 'Albums released after 1991' AS 'CAPTION';
+DELETE FROM albums WHERE release_date > 1991;
+SELECT 'Albums with the genre disco' AS 'CAPTION';
+DELETE FROM albums WHERE genre = 'disco';
+SELECT 'Albums by ''Meat Loaf''' AS 'CAPTION';
+DELETE FROM albums WHERE artist = 'Meat Loaf';
