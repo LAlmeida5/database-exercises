1a5edfd580bfe8b0d94a429a1103da3e3b6006e0
Luis Almeida <luis.a.almeidajr@gmail.com>
2022-07-11 16:56:13 -0500

    EOD

---
+++ subqueries_exercises.sql
@@ -0,0 +1,29 @@
+USE employees;
+
+SELECT CONCAT(first_name,' ',last_name)
+FROM employees
+WHERE hire_date IN (
+    SELECT hire_date
+    FROM employees
+    WHERE emp_no = '101010'
+    );
+
+
+SELECT title, COUNT(emp_no)
+FROM titles
+WHERE emp_no IN (
+    SELECT emp_no
+    FROM employees
+    WHERE first_name = 'Aamod'
+    )
+GROUP BY title;
+
+SELECT first_name, last_name
+FROM employees
+WHERE emp_no IN (
+    SELECT emp_no
+    FROM dept_manager
+    WHERE to_date = '9999-01-01'
+    ) AND  gender = 'F';
+
+
